715314c8db68c8c055f72a789626bb187cd9ba89
Benjamin Shu <bshu005@wch136-24.cs.ucr.edu>
2021-04-11 13:37:36 -0700

    wip lab3 part 1

--- source/main.c
+++ source/main.c
@@ -15,29 +15,65 @@
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
-    DDRC = 0x00; PORTC = 0xFF;
-    DDRD = 0xFF; PORTD = 0x00; //Port D = output, initialize ports as 0
+    DDRC = 0xFF; PORTC = 0x00;
     unsigned char tempA = 0x00;
     unsigned char tempB = 0x00;
-    unsigned char tempC = 0x00;
-    unsigned char tempD0 = 0x00;	
-    unsigned char tempD1 = 0x00;
-    unsigned short totalWeight = 0x0000;
+    unsigned char count = 0;
 	while(1) {
 		// 1) Read input
-		
-        tempA = PINA;
+		tempA = PINA;
         tempB = PINB;
-        tempC = PINC;
-        
-        totalWeight = tempA + tempB + tempC;
-        if(totalWeight > 140) { //if weight exceeds 140 kg, set PD0 to 1
-            tempD0 = tempD0 | 0x01;
+        count = 0;
+        if((tempA & 0x01) == 0x01) {
+            count = count + 1;
         }
-        if((PINA - PINC) > 80 || (PINC - PINA) > 80) {
-            tempD1 = tempD1 | 0x02;
-        }  
-        PORTD = (totalWeight & 0x00FC) | tempD0 | tempD1;
+        if((tempA & 0x02) == 0x02) {
+            count = count + 1;
+        }
+        if((tempA & 0x04) == 0x04) {
+            count = count + 1;
+        }
+        if((tempA & 0x08) == 0x08) {
+            count = count + 1;
+        }
+        if((tempA & 0x10) == 0x10) {
+            count = count + 1;
+        }
+        if((tempA & 0x20) == 0x20) {
+            count = count + 1;
+        }
+        if((tempA & 0x40) == 0x40) {
+            count = count + 1;
+        }
+        if((tempA & 0x80) == 0x80) {
+            count = count + 1;
+        }
+        if((tempB & 0x01) == 0x01) {
+            count = count + 1;
+        }
+        if((tempB & 0x02) == 0x02) {
+            count = count + 1;
+        }
+        if((tempB & 0x04) == 0x04) {
+            count = count + 1;
+        }
+        if((tempB & 0x08) == 0x08) {
+            count = count + 1;
+        }
+        if((tempB & 0x10) == 0x10) {
+            count = count + 1;
+        }
+        if((tempB & 0x20) == 0x20) {
+            count = count + 1;
+        }
+        if((tempB & 0x40) == 0x40) {
+            count = count + 1;
+        }
+        if((tempB & 0x80) == 0x80) {
+            count = count + 1;
+        }
+        PORTC = count;   
+        
 	}
 	return 1;
 }
